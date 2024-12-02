65bb9c68eee02da508bc5cc98dcc97ab355adfff
“Mebu98” <merijn.buitelaar98@gmail.com>
2024-12-02 19:14:08 +0100

    day02 part 01

---
+++ day02/solution.py
@@ -0,0 +1,32 @@
+puzzleInput = open("input.txt", "r")
+puzzleRows = puzzleInput.read().split("\n")
+answer = 0
+
+## Split each row into an array of "numbers" (currently still strings)
+for i, row in enumerate(puzzleRows):
+    puzzleRows[i] = row.split()
+
+for row in puzzleRows:
+    startJump = (int(row[1]) - int(row[0]))
+    maxJump = 3
+    safe, isAscending = True, True
+    if 0 > startJump: isAscending = False
+
+    for i in range(1, len(row)):
+        prev, curr = int(row[i-1]), int(row[i])
+        currJump = (curr - prev)
+        currIsAscending = True
+        if 0 > currJump: currIsAscending = False
+
+        if currJump == 0 or isAscending != currIsAscending:
+            safe = False
+            break
+
+        if abs(curr - prev) > maxJump:
+            safe = False
+            break
+
+    if safe:
+        answer += 1
+
+print(answer)
