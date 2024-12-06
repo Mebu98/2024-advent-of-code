bbc3fbb1dfb9a851a8566c43a9c1b00dfb9d4b06
“Mebu98” <merijn.buitelaar98@gmail.com>
2024-12-06 17:28:08 +0100

    refactor day02-02 to satisfy PEP 8 conventions

--- day02/solution2.py
+++ day02/solution2.py
@@ -1,52 +1,52 @@
-def ReadFile():
-    returnRows = []
-    puzzleInput = open("input.txt", "r").read().split("\n")
-    for row in puzzleInput:
-        rowAsInt = []
+def read_file():
+    return_rows = []
+    puzzle_input = open("input.txt", "r").read().split("\n")
+    for row in puzzle_input:
+        row_as_int = []
         row = row.split()
-        for num in row: rowAsInt.append(int(num))
-        returnRows.append(rowAsInt)
+        for num in row: row_as_int.append(int(num))
+        return_rows.append(row_as_int)
 
-    return returnRows
+    return return_rows
 
 def Start():
-    inputRows = ReadFile()
+    inputRows = read_file()
     count = 0
 
     for row in inputRows:
-        rowIsValid = AdvancedRowIsValid(row)
-        count += rowIsValid
+        this_row_is_valid = advanced_row_is_valid(row)
+        count += this_row_is_valid
 
     print(count)
     return count
 
-def AdvancedRowIsValid(row):
+def advanced_row_is_valid(row):
     # If row is valid without removing any number, return True
-    if RowIsValid(row): return True
+    if row_is_valid(row): return True
 
     # Else try popping index i for every index and check if the row is valid without it
     # (Very inefficient :D)
     for i in range(0, len(row)):
         subRow = row.copy()
         subRow.pop(i)
-        if RowIsValid(subRow): return True
+        if row_is_valid(subRow): return True
 
     return False
 
-def RowIsValid(row):
-    isAsc = row[1] > row[0]
+def row_is_valid(row):
+    is_asc = row[1] > row[0]
     for i in range(1, len(row)):
-        currNum = row[i]
-        prevNum = row[i - 1]
-        currIsAsc = currNum > prevNum
+        curr_num = row[i]
+        prev_num = row[i - 1]
+        curr_is_asc = curr_num > prev_num
 
-        if currNum == prevNum:
+        if curr_num == prev_num:
             return False
 
-        if isAsc != currIsAsc:
+        if is_asc != curr_is_asc:
             return False
 
-        if abs(currNum - prevNum) > 3:
+        if abs(curr_num - prev_num) > 3:
             return False
 
     return True
